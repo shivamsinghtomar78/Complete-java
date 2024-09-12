fa6d8c3bad86a5e4fd45a375bdc715684183b7c7
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-09-12 20:14:45 +0530

    Add files via upload

---
+++ DSA with JAVA/Basic_dsaWithJava_assignmentQ/Question_1.java
@@ -0,0 +1,10 @@
+package Basic_dsaWithJava_assignmentQ;
+
+public class Question_1 {
+    public static void main(String[] args) {
+        //Q.Explain the purpose of the escape sequence \n in Java. Provide an example of its usage in a program,
+        //and predict the output.
+        System.out.println("Hello,World\nJava is great");
+    }
+}
+
---
+++ DSA with JAVA/basicJava_Q/pridictAnswer1.java
@@ -0,0 +1,10 @@
+package basicJava_Q;
+
+public class pridictAnswer1 {
+    public static void main(String[] args) {
+        int x=1;
+        while(x==1)
+            x=x-1;
+        System.out.println(x);
+    }
+}
